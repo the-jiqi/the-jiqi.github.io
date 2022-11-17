a7778e249ced58a2bbc1da607705bb44e4abbf2d
the-jiqi <the.jiqi@gmail.com>
2022-11-17 16:34:34 +0000

    fixed code

--- process-form.php
+++ process-form.php
@@ -11,30 +11,4 @@ if ( ! $terms) {
     die("Terms must be accepted");
 }
 
-$host = "localhost";
-$dbname = "message_db";
-$username = "root";
-$password = "";
-
-$conn = mysqli_connect($host, $username, $password, $dbname);
-
-if (mysqli_connect_error()) {
-    die("Connection error: " .mysqli_connect_error);
-}
-
-
-$sql = "INSERT INTO message (name, body, priority, type)
-        VALUES (?,?,?,?)";
-
-$stmt = mysqli_stmt_init($conn);
-
-if (! mysqli_stmt_prepare($stmt, $sql)) {
-    die(mysqli_error($conn));
-}
-
-
-mysqli_stmt_bind_param($stmt, 'ssii', $name, $message, $priority, $type);
-
-mysqli_stmt_execute($stmt);
-
-echo "Record saved"
+var_dump($name, $message, $priority, $type, $terms);
